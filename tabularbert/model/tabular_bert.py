de58f3bb3bcd6ae7972e2d493af34ffe86c391e8
bbbeomjin@gmail.com <bbbeomjin@gmail.com>
2026-06-29 20:22:25 +0900

    fix: Fix minor errors

--- tabularbert/model/tabular_bert.py
+++ tabularbert/model/tabular_bert.py
@@ -1294,8 +1294,8 @@ class TabularBERTTrainer(nn.Module):
         config = torch.load(save_path)
         
         # Load discretizer
-        discretizer = UniformDiscretize(config['data']['num_bins'], 
-                                        config['data']['encoding_info'])
+        discretizer = UniformDiscretize(config['data_config']['num_bins'], 
+                                        config['data_config']['encoding_info'])
         discretizer.bins = config['discretizer']['bins']
         discretizer.category_maps = config['discretizer']['category_maps']
         
@@ -1405,8 +1405,8 @@ class TabularBERTPredictor:
         
         # Load discretizer
         discretizer = UniformDiscretize(
-            config['data']['num_bins'], 
-            config['data']['encoding_info']
+            config['data_config']['num_bins'], 
+            config['data_config']['encoding_info']
         )
         discretizer.bins = config['discretizer']['bins']
         discretizer.category_maps = config['discretizer']['category_maps']
